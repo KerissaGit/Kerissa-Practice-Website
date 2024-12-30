966b1bbbb7e817207cea5e1df4bbcf3585681e1a
KerissaGit <kerissa525@gmail.com>
2024-12-29 21:50:55 -0500

    Updated look. Corrected issues in Css

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 
-document.querySelector("body").appendChild(h2);
+document.getElementById('changeColorBtn').addEventListener('click', function() {
+    const colorBox = document.getElementById('colorBox');
+    colorBox.style.backgroundColor = colorBox.style.backgroundColor === 'blue' ? 'pink' : 'blue';
+  });
+  
